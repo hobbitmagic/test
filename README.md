e179592b3d9512157fe6f240e4e397e8364411f0
hobbitmagic <151260692+hobbitmagic@users.noreply.github.com>
2023-11-17 16:19:27 -0700

    Implemented encode and decode

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
-# test
+# URL Shortener
 
-Don't want to disclose too much since this is a public repo so will keep this as generic as possible.
+A simple url shortener. API consisting of two GET endpoints: /encode and /decode.
+Input: takes required "url" string parameter for each endpoint.
+Output: JSON object with "short" or "long" properties.
+Stores urls in memory, so cannot be scaled much, and any restart erases all but one example url.
+
+#### Sidenote
+Limited test to GET method, but can easily be expanded to POST and/or other methods as needed.
+Also since this is only three functions, decided against creating a class.
+If expanded further, recommend making a class and a module. For now manual tests, but considering automating them.
 
 ## Requirements
 - Python 3 (used 3.11)
 - Pip (used 23.3.1)
 
 ## Usage
-1. Install requirements: `pip install -r requirements.txt`
+1. Install dependencies: `pip install -r requirements.txt`
 1. Then run the app `flask --app main run`
-1. Use the localhost address from above command to test like: "http://127.0.0.1:5000/encode?url=http://example.com/dir1/dir2" or "http://127.0.0.1:5000/decode?url=http://example.com/AbCd7E"
+
+## Tests
+1. Use the localhost address from above command to test
+1. Encode existing example url "http://127.0.0.1:5000/encode?url=http://example.com/dir1/dir2"
+1. Decode existing example url "http://127.0.0.1:5000/decode?url=http://example.org/AbCd7E"
+1. Encode non-existing url "http://127.0.0.1:5000/encode?url=http://example.com/dir1/dir2"
+1. Decode non-existing url "http://127.0.0.1:5000/decode?url=doesnotexist"
+1. Encode error missing url "http://127.0.0.1:5000/encode"
+1. Decode error missing url "http://127.0.0.1:5000/decode"
--- main.py
+++ main.py
@@ -1,11 +1,58 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import string, random
+
+# Options
+short_url_length = 6
 
 api = Flask(__name__)
 
-@api.route("/encode")
-def encode():
-    return "Encoded"
+memory_store = [
+    {"long": "http://example.com/dir1/dir2", "short": "http://example.org/AbCd7E"}
+]
 
-@api.route("/decode")
+def generate_short_url():
+    new_short_url = ''.join(random.choices(
+        string.ascii_uppercase +
+        string.ascii_lowercase +
+        string.digits
+        , k=short_url_length))
+    # Check if exists, and generate another if it already does
+    if next((item for item in memory_store if item["short"] == new_short_url), None):
+        return generate_short_url()
+    else:
+        return "http://example.org/" + new_short_url
+
+
+# Encode route
+# Input Parameters:
+#   url (string) (required) - long url user wants to encode
+# Output: (json object) - short url {"short": "http://example.org/ABCDEF"}
+@api.route("/encode", methods=['GET'])
+def encode():
+    passed_url = request.args.get('url', None)
+    if (passed_url is None):
+       return jsonify({"error": "Missing url parameter"})
+    # Check for existing, and return existing if found. Otherwise create and save new.
+    existing_match = next((item["short"] for item in memory_store if item["long"] == passed_url), None)
+    if (existing_match):
+        return jsonify({"short": existing_match})
+    else:
+        new_short_url = generate_short_url()
+        memory_store.append({"long": passed_url, "short": new_short_url })
+        return jsonify({"short": new_short_url })
+
+# Decode route
+# Input Parameters:
+#   url (string) (required) - short url user wants to decode
+# Output: (json object) - long url {"long": "http://example.com/dir1/dir2"}
+@api.route("/decode", methods=['GET'])
 def decode():
-    return "Decoded"
+    passed_url = request.args.get('url', None)
+    if (passed_url is None):
+       return jsonify({"error": "Missing url parameter"})
+    # Check for existing, and return existing if found. Otherwise error out.
+    existing_match = next((item["long"] for item in memory_store if item["short"] == passed_url), None)
+    if (existing_match):
+        return jsonify({"long": existing_match})
+    else:
+        return jsonify({"error": "Could not find shortened url"})
